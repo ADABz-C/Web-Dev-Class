efd4fe598182ba521c0be9eb469c50a1596c89a8
Chidi <adabanyadavid@gmail.com>
2026-02-23 00:02:39 -0500

    task 1

--- AdvancedArrayOperations-MusicAnalytics/solutions.js
+++ AdvancedArrayOperations-MusicAnalytics/solutions.js
@@ -1,5 +1,39 @@
-import {listeningData} from data.js;
+import {listeningData} from './data.js';
 
 const task1answer = document.getElementById(task1Answer);
 
-const 
+const totalStreams = listeningData.reduce((acc, songs) => {
+    
+    if (songs.artist in acc) {
+        acc[songs.artist] = acc[songs.artist] + songs.streams;
+    } else {
+        acc[songs.artist] = songs.streams;
+    }
+    return acc;
+
+
+},{})
+
+const artistarray = Object.entries(totalStreams);
+console.log(artistarray); 
+
+const streamRankings = artistarray.map(([artist, streams]) => {
+    let tier;
+
+    if(streams>= 10000000){
+        tier = 'Platinum';
+    }
+    else if (streams >= 5000000){
+        tier = 'Gold';
+    }
+    else {
+        tier = 'Silver';
+    }
+    return {
+        artist: artist,
+        totalStreams: streams,
+        tier: tier
+    };
+});
+
+task1answer = 
